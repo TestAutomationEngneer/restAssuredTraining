28b549d5c480aa17d95e814ef93665879d25f51b
Dariusz Hryciuk <dariusz.hryciuk@epay.ingenico.com>
2022-11-24 17:36:54 +0100

    add requestSpecification and responsespecification plus plik konfiguracyjny config.yaml

---
+++ src/test/java/requestResponseTask/WeatherRequest.java
@@ -0,0 +1,14 @@
+package requestResponseTask;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+
+public class WeatherRequest {
+
+    public static RequestSpecification getWeatherInfoByCityID(int id) {
+        return new RequestSpecBuilder()
+                .addParam("id", id)
+                .build();
+    }
+
+}
